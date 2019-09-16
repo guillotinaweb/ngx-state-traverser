b4732308a7894e4455a4ee3ae523e14e2f7f7f26
Tilda <mathilde@onna.com>
2019-09-16 16:09:13 +0200

    Add getAncestors selector

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 1.0.1 (2019-09-26)
+
+- Add getAncestors selector
+
 # 1.0.0 (2019-09-04)
 
 - Initial version
--- projects/ngx-state-traverser/src/lib/selector.ts
+++ projects/ngx-state-traverser/src/lib/selector.ts
@@ -1,18 +1,20 @@
-import { createFeatureSelector, createSelector, select, MemoizedSelector, Store } from '@ngrx/store';
+import { createFeatureSelector, createSelector, select, Store } from '@ngrx/store';
 import { TraversingState, TraversingStateFeatures } from './state';
 import { Target } from 'angular-traversal';
-import { map, filter } from 'rxjs/operators';
+import { map, filter, tap } from 'rxjs/operators';
 import { TraverserActions } from './actions';
+import { Observable } from 'rxjs';
 
 export namespace TraverserSelectors {
     export class Missing {
         path: string;
+
         constructor(path: string) {
             this.path = path;
         }
-    };
+    }
 
-    type ContextOrMissing = {[key: string]: any} | Missing;
+    type ContextOrMissing = { [key: string]: any } | Missing;
 
     function _getParentPath(state: TraversingState): string {
         let targetPath = state.target.contextPath;
@@ -53,7 +55,7 @@ export namespace TraverserSelectors {
 
     export const getContext = createSelector(
         traversalSelector,
-        (state: TraversingState): {[key: string]: any} => state.target.context
+        (state: TraversingState): { [key: string]: any } => state.target.context
     );
 
     export function TraverserContext<T>(store: Store<any>) {
@@ -77,6 +79,36 @@ export namespace TraverserSelectors {
             return state.collection[parentPath] || new Missing(parentPath);
         }
     );
+
+    export const getAncestors = createSelector(
+        traversalSelector,
+        (state: TraversingState, path: string): ContextOrMissing[] => {
+            const ancestorPaths = path.split('/').filter(chunk => !!chunk).reduce((allChunks, chunk) => {
+                if (allChunks.length === 0) {
+                    allChunks.push(`/${chunk}`);
+                } else {
+                    allChunks.push(`${allChunks[allChunks.length - 1]}/${chunk}`);
+                }
+                return allChunks;
+            }, [] as string[]);
+
+            const ancestors: ContextOrMissing[] = ancestorPaths.map(ancestor => state.collection[ancestor] || new Missing(ancestor));
+            return ancestors.filter(ancestor => !ancestor['isForbidden']);
+        }
+    );
+
+    export function TraverseToAncestors<T>(store: Store<any>, path: string): Observable<(T | undefined)[]> {
+        return store.pipe(
+            select(getAncestors, path),
+            tap(ancestors => ancestors.forEach(ancestor => {
+                if (ancestor instanceof Missing) {
+                    store.dispatch(new TraverserActions.Traverse(ancestor.path));
+                }
+            })),
+            filter(ancestors => ancestors.every(ancestor => !(ancestor instanceof Missing))),
+            map(ancestors => ancestors as T[])
+        );
+    }
 
     export function TraverseToParent<T>(store: Store<any>) {
         return store.pipe(
